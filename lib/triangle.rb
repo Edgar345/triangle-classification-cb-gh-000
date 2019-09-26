ea9d6253275640d82326cdeaa871d7781d364974
Edgar V <evenegas9@ucmerced.edu>
2019-09-26 05:17:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,9 @@
 class Triangle
+  attr_accessor :length_1, :length_2, :length_3
+  
   def initialize length_1, length_2, length_3
-  end
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
   end
 end
